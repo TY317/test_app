dfd7ec7723f7b6fc47f1e5255172cd306de600cb
TY317 <musicman_stingray_f91@yahoo.co.jp>
2024-06-16 15:26:43 +0900

    入力フォームの作成

--- pages/AT初当り.py
+++ pages/AT初当り.py
@@ -11,18 +11,27 @@ import streamlit as st
 st.header("初当りのデータメモ")
 
 ##### 初当りのデータを入力し、登録するフォーム
-# 3列のカラムを作成
-col1, col2, col3 = st.columns(3)
+with st.form(key='bonus_data_input'):
 
-#当選ゲーム数の入力
-with col1:
-    # st.subheader("ゲーム数")
-    game_num = st.number_input(label="ゲーム数")
+    # 3列のカラムを作成
+    col1, col2, col3 = st.columns(3)
 
-#推測モードの入力
-with col2:
-    st.subheader("推測モード")
+    #当選ゲーム数の入力
+    with col1:
+        # st.subheader("ゲーム数")
+        game_num = st.number_input(label="ゲーム数", step=1)
 
-#当選契機の入力
-with col3:
-    st.subheader("当選契機")
+    #推測モードの入力
+    with col2:
+        # st.subheader("推測モード")
+        mode = st.multiselect("推測モード",
+                            ["天国","通常","地獄","?"])
+
+    #当選契機の入力
+    with col3:
+        # st.subheader("当選契機")
+        koyaku = st.multiselect("当選契機",
+                                ["中チェ","強スイカ","チャンス目","弱スイカ","角チェ","確定役","謎"])
+        
+    #登録ボタン
+    submit_btn = st.form_submit_button("登録")
