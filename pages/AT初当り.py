5040875f330018493cbc30cab6de104d6980d1ba
TY317 <musicman_stingray_f91@yahoo.co.jp>
2024-06-22 11:16:21 +0900

    エラー修正など

--- pages/AT初当り.py
+++ pages/AT初当り.py
@@ -30,6 +30,9 @@ with st.form(key='bonus_data_input'):
         # st.subheader("推測モード")
         mode = st.multiselect(df.columns[1],
                             ["天国","通常","地獄","?"])
+        
+        #1つの文字列にしておく
+        joined_mode = ",".join(mode)
 
     #当選契機の入力
     with col3:
@@ -37,15 +40,18 @@ with st.form(key='bonus_data_input'):
         koyaku = st.multiselect(df.columns[2],
                                 ["中チェ","強スイカ","チャンス目","弱スイカ","角チェ","確定役","謎", "天井"])
         
+        #1つの文字列にしておく
+        joined_koyaku = ",".join(koyaku)
+        
     #登録ボタン
     submit_btn = st.form_submit_button("登録")
 
     ##### 登録ボタン押されたらデータフレームにデータ追加
     if submit_btn:
         #入力されたデータをデータフレームにする
-        df_new = pd.DataFrame({df.columns[0]: game_num,
-                               df.columns[1]: mode,
-                               df.columns[2]: koyaku})
+        df_new = pd.DataFrame({df.columns[0]: [game_num],
+                               df.columns[1]: [joined_mode],
+                               df.columns[2]: [joined_koyaku]})
         
         #すでにあるデータと入力されたデータを合体させたデータフレームにする
         df = pd.concat([df, df_new])
@@ -57,24 +63,49 @@ with st.form(key='bonus_data_input'):
 st.dataframe(df)
 
 ##### 初当り確率の算出と解析データの表示
+st.subheader("AT初当り確率")
 
 #2列に画面分割
 col1, col2 = st.columns(2)
 
 #初当り確率の算出
 with col1:
+    st.caption("現在値")
     #ゲーム数カラム内のデータの数を取得
     at_times = int(df[df.columns[0]].count())
 
-    #初当りゲーム数の合計値を取得
-    sum_games = int(df[df.columns[0]].sum())
+    #初当りが0回ならスキップ
+    if at_times > 0:
+        #初当りゲーム数の合計値を取得
+        sum_games = int(df[df.columns[0]].sum())
 
-    #平均初当りゲーム数を算出
-    ave_games = int(sum_games / at_times)
+        #平均初当りゲーム数を算出
+        ave_games = int(sum_games / at_times)
 
-    #算出結果の表示
-    st.info(f"1/{ave_games}")
+        #算出結果の表示
+        st.info(f"1/{ave_games}")
+    
+    else:
+        pass
 
 #解析データの表示
 with col2:
-    st.image("./pictures/AT初当り確率.bmp")
+    # st.subheader("AT初当り確率")
+    st.caption("解析値")
+
+    #####表をデータフレームで作成
+    #カラム名を設定
+    colomn_name = ["バトルボーナス初当り"]
+
+    #インデックス名を設定
+    index_name = ["設定1", "設定2", "設定4", "設定5", "設定6"]
+
+    #確率データを設定
+    bb_data = ["1/383.4", "1/370.5", "1/297.8", "1/258.7", "1/235.1"]
+
+    #データフレーム作成
+    bb_df = pd.DataFrame(bb_data, index=index_name, columns=colomn_name)
+
+    #データフレームの表示
+    st.dataframe(bb_df)
+    # st.image("./pictures/AT初当り確率.bmp")
