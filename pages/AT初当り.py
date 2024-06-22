7e2d27453f7b02c13e0ce4e6f578e306251d12dc
TY317 <musicman_stingray_f91@yahoo.co.jp>
2024-06-22 10:30:06 +0900

    csvの機能を追加

--- pages/AT初当り.py
+++ pages/AT初当り.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 ##### ページの内容 #####
 # ATの当選ゲーム数と契機をメモ
@@ -7,11 +8,14 @@ import streamlit as st
 ##############################
 ##### 当選ゲーム数と契機のメモ
 ##############################
-#見出し
-st.header("初当りのデータメモ")
+
+#csvファイルを読み込み、データフレームに格納する
+df = pd.read_csv("./pages/AT_get_df.csv")
 
 ##### 初当りのデータを入力し、登録するフォーム
 with st.form(key='bonus_data_input'):
+
+    st.caption("初当りデータ入力")
 
     # 3列のカラムを作成
     col1, col2, col3 = st.columns(3)
@@ -19,19 +23,58 @@ with st.form(key='bonus_data_input'):
     #当選ゲーム数の入力
     with col1:
         # st.subheader("ゲーム数")
-        game_num = st.number_input(label="ゲーム数", step=1)
+        game_num = st.number_input(label=df.columns[0], step=1)
 
     #推測モードの入力
     with col2:
         # st.subheader("推測モード")
-        mode = st.multiselect("推測モード",
+        mode = st.multiselect(df.columns[1],
                             ["天国","通常","地獄","?"])
 
     #当選契機の入力
     with col3:
         # st.subheader("当選契機")
-        koyaku = st.multiselect("当選契機",
-                                ["中チェ","強スイカ","チャンス目","弱スイカ","角チェ","確定役","謎"])
+        koyaku = st.multiselect(df.columns[2],
+                                ["中チェ","強スイカ","チャンス目","弱スイカ","角チェ","確定役","謎", "天井"])
         
     #登録ボタン
     submit_btn = st.form_submit_button("登録")
+
+    ##### 登録ボタン押されたらデータフレームにデータ追加
+    if submit_btn:
+        #入力されたデータをデータフレームにする
+        df_new = pd.DataFrame({df.columns[0]: game_num,
+                               df.columns[1]: mode,
+                               df.columns[2]: koyaku})
+        
+        #すでにあるデータと入力されたデータを合体させたデータフレームにする
+        df = pd.concat([df, df_new])
+
+        #csvに保存する
+        df.to_csv("./pages/AT_get_df.csv", index=False)
+
+#当選履歴データの表示
+st.dataframe(df)
+
+##### 初当り確率の算出と解析データの表示
+
+#2列に画面分割
+col1, col2 = st.columns(2)
+
+#初当り確率の算出
+with col1:
+    #ゲーム数カラム内のデータの数を取得
+    at_times = int(df[df.columns[0]].count())
+
+    #初当りゲーム数の合計値を取得
+    sum_games = int(df[df.columns[0]].sum())
+
+    #平均初当りゲーム数を算出
+    ave_games = int(sum_games / at_times)
+
+    #算出結果の表示
+    st.info(f"1/{ave_games}")
+
+#解析データの表示
+with col2:
+    st.image("./pictures/AT初当り確率.bmp")
--- test.py
+++ test.py
@@ -1,5 +1,24 @@
 import streamlit as st
+import pandas as pd
 
 st.title("スマスロ北斗の拳")
+
+##### 新規作成ボタンを押すとデータをすべてリセットする
+
+#フォームの作成
+with st.form(key='new_play'):
+
+    #新規作成ボタンの設定
+    start_btn = st.form_submit_button("新規作成")
+
+    #ボタンが押されたらcsvファイルをリセットし保存
+    if start_btn:
+
+        ##### AT初当り用
+        #AT初当り用のデータフレームを保存するcsvファイル
+        AT_get_df = pd.DataFrame(columns=["ゲーム数", "推測モード", "当選契機"])
+        # AT_get_df = pd.DataFrame(columns=["a", "b", "c"])
+        AT_get_df.to_csv("./pages/AT_get_df.csv", index=False)
+
 st.caption("ver1.0.0")
 st.caption("   ・新規作成")
